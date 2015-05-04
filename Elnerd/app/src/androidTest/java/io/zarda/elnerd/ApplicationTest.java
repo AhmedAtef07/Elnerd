b0476ff3631454c2eab0e2012a003ffe40a6aa41
Ahmed_Emad <7MADAH.1994@Gmail.com>
2015-05-04 16:21:39 +0300

    Make empty andriod project.

---
+++ Elnerd/app/src/androidTest/java/io/zarda/elnerd/ApplicationTest.java
@@ -0,0 +1,13 @@
+package io.zarda.elnerd;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
