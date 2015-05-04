ad4f11d88a92858841db21c78c41ad4ce127f407
Ahmed_Emad <7MADAH.1994@Gmail.com>
2015-05-04 18:00:04 +0300

    Prepare '*View' classes for implementation.

--- Elnerd/app/src/main/java/io/zarda/elnerd/view/GameView.java
+++ Elnerd/app/src/main/java/io/zarda/elnerd/view/GameView.java
@@ -1,7 +1,24 @@
 package io.zarda.elnerd.view;
 
+import android.content.Context;
+
 /**
  * Created by atef & emad on 4 May, 2015.
+ * Implementing by magdy.
  */
-public class GameView {
+public class GameView implements Viewable {
+    @Override
+    public void initializeView(Context context) {
+
+    }
+
+    @Override
+    public void startView() {
+
+    }
+
+    @Override
+    public void endView() {
+
+    }
 }
--- Elnerd/app/src/main/java/io/zarda/elnerd/view/HomeView.java
+++ Elnerd/app/src/main/java/io/zarda/elnerd/view/HomeView.java
@@ -1,7 +1,24 @@
 package io.zarda.elnerd.view;
 
+import android.content.Context;
+
 /**
  * Created by atef & emad on 4 May, 2015.
+ * Implementing by magdy.
  */
-public class HomeView {
+public class HomeView implements Viewable {
+    @Override
+    public void initializeView(Context context) {
+
+    }
+
+    @Override
+    public void startView() {
+
+    }
+
+    @Override
+    public void endView() {
+
+    }
 }
