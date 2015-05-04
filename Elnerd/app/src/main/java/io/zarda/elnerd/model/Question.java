2ba6e1f9b6bceb82a3c17654b5489b88956dd465
Ahmed_Emad <7MADAH.1994@Gmail.com>
2015-05-04 18:09:51 +0300

    Add main attributes to 'Question' class.

--- Elnerd/app/src/main/java/io/zarda/elnerd/model/Question.java
+++ Elnerd/app/src/main/java/io/zarda/elnerd/model/Question.java
@@ -1,7 +1,30 @@
 package io.zarda.elnerd.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by atef & emad on 4 May, 2015.
  */
 public class Question {
+    private String header;
+    private ArrayList<String> choices;
+    private int correctIndex;
+
+    public Question(String header, ArrayList<String> choices, int correctIndex) {
+        this.header = header;
+        this.choices = choices;
+        this.correctIndex = correctIndex;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public int getCorrectIndex() {
+        return correctIndex;
+    }
 }
