67a3c7be26c029063b0c0d7212fa02ed15120605
ahmedmagdy711 <ahmedmagdy711@gmail.com>
2015-05-07 10:54:15 +0200

    simple HomeView

--- Elnerd/app/src/main/java/io/zarda/elnerd/view/HomeView.java
+++ Elnerd/app/src/main/java/io/zarda/elnerd/view/HomeView.java
@@ -1,27 +1,55 @@
 package io.zarda.elnerd.view;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import java.util.List;
+
+import io.zarda.elnerd.R;
 
 /**
  * Created by atef & emad on 4 May, 2015.
  * Implementing by magdy.
  */
 public class HomeView implements Viewable {
+
+    Context context;
+
+    private FrameLayout mainLayout;
+
+    private Button play;
+
     @Override
     public void initializeView(Context context , List<View> views) {
+        this.context = context;
 
+        play = (Button) (views.get(0));
     }
 
     @Override
     public void startView() {
-
+        ((Activity)context).setContentView(mainLayout);
     }
 
     @Override
     public void endView() {
+        ((ViewGroup) mainLayout).removeAllViews();
+    }
 
+    private  void setLayout(){
+        mainLayout = new FrameLayout(context);
+    }
+
+    private void setButtons(){
+        play.setBackground(context.getResources().getDrawable(R.drawable.display3));
+        play.setTextColor(Color.parseColor("#ecf0f1"));
+        mainLayout.addView(play);
+        play.setGravity(Gravity.CENTER);
     }
 }
