e77a55d15226fcd10f2f59c4cb9f40be4fed5d7c
Ahmed-Emad <7MADAH.1994@Gmail.com>
2015-05-12 01:32:26 +0300

    Add 'Quote' class and 'User' class and Add category and mode.

--- Elnerd/app/src/main/java/io/zarda/elnerd/model/Quote.java
+++ Elnerd/app/src/main/java/io/zarda/elnerd/model/Quote.java
@@ -9,4 +9,52 @@ public class Quote {
     private int userId;
     private int bookId;
     private int categoryId;
+
+    public Quote(String content, int id, int userId, int bookId, int categoryId) {
+        this.content = content;
+        this.id = id;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.categoryId = categoryId;
+    }
+
+    public Quote(String content) {
+        this.content = content;
+        this.id = 0;
+        this.userId = 0;
+        this.bookId = 0;
+        this.categoryId = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 }
